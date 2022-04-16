95af4bbf54e5a0217331d32b26724cbd5eeece1f
SteveLiu <fe-hawk@outlook.com>
2022-04-17 00:12:17 +0800

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by SteveLiu on 2022/4/17.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
