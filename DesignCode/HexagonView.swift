8fbb148635b6548ae66df4e4a0272517eca9270d
steveliu <fe-hawk@outlook.com>
2023-01-05 23:04:05 +0800

    [feat] 引入CanvasAPI 重构 HexagonView

--- DesignCode/HexagonView.swift
+++ DesignCode/HexagonView.swift
@@ -10,10 +10,25 @@ import SwiftUI
 struct HexagonView: View {
     var body: some View {
         Canvas{ context, size in
-            context.draw(Text("DesignCode"), at: CGPoint(x: 50, y: 20))
-            context.draw(Image("Blob 1"), in: CGRect(x: 0, y: 0, width: 200, height: 200))
-            context.draw(Image(systemName: "hexagon.fill"), in: CGRect(x: 100, y: 100, width: 200, height: 200))
+            // 使用Canvas API可以高效地创建可交互的动画，同时保证性能
+            context.draw(
+                Text("DesignCode")
+                    .font(.largeTitle),
+                at: CGPoint(x: 50, y: 20))
+            context.fill(
+                Path(ellipseIn: CGRect(x: 0, y: 0, width: 100, height: 100)),
+                with: .color(.blue))
+            context.draw(
+                Image("Blob 1"),
+                in: CGRect(x: 0, y: 0, width: 200, height: 200))
+            context.draw(
+                Image(systemName: "hexagon.fill"),
+                in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         }
+        .frame(width: 200, height: 212)
+        .foregroundStyle(.linearGradient(colors: [.pink, .blue],
+                                         startPoint: .topLeading,
+                                         endPoint: .bottomTrailing))
     }
 }
 
